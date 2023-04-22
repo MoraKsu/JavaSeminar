d795b6ee067c8943e251ffd89c9626ff5c82b5ca
Kseniya Erina <moraksu@gmail.com>
2023-04-22 12:50:27 +0400

    add task_4

--- src/Seminar03HomeWork/task_4.java
+++ src/Seminar03HomeWork/task_4.java
@@ -12,42 +12,51 @@ import java.util.Arrays;
 
 public class task_4 {
     public static void main(String[] args) {
-        int[] sortArr = {12, 6, 4, 1, 15, 10};
-        int[] result = mergeSort(sortArr);
-        System.out.println(Arrays.toString(result));
-    }
-    public static int[] mergeSort(int[] sortArr) {
-        int[] buffer1 = Arrays.copyOf(sortArr, sortArr.length);
-        int[] buffer2 = new int[sortArr.length];
-        int[] result = mergeSortInner(buffer1, buffer2, 0, sortArr.length);
-        return result;
+        int[] arr = { 5, 2, 8, 3, 1, 6, 9, 4, 7 };
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static int[] mergeSortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
-        if (startIndex >= endIndex - 1) {
-            return buffer1;
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
         }
 
-        //уже отсортирован
-        int middle = startIndex + (endIndex - startIndex) / 2;
-        int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
-        int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
+        while (i <= mid) {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
 
-        //слияние
-        int index1 = startIndex;
-        int index2 = middle;
-        int destIndex = startIndex;
-        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
-        while (index1 < middle && index2 < endIndex) {
-            result[destIndex++] = sorted1[index1] < sorted2[index2]
-                    ? sorted1[index1++] : sorted2[index2++];
+        while (j <= right) {
+            temp[k] = arr[j];
+            j++;
+            k++;
         }
-        while (index1 < middle) {
-            result[destIndex++] = sorted1[index1++];
+
+        for (k = 0; k < temp.length; k++) {
+            arr[left + k] = temp[k];
         }
-        while (index2 < endIndex) {
-            result[destIndex++] = sorted2[index2++];
-        }
-        return result;
     }
 }
